5742b7facad532ede04261e36b551d7371ae691a
Muhammad Arif <arif1951995@gmail.com>
2019-07-11 12:08:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,291 @@
+let whatsAppData = [
+    {
+        name: "a",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+
+
+    },
+    {
+        name: "b",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "c",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "d",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "e",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "f",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "g",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "h",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "i",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "j",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "k",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "l",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "m",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "n",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "o",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "p",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "q",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "r",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "s",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "t",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "u",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "v",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "w",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "x",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "y",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }, 
+    {
+        name: "z",
+        number: "038038486",
+        lastMsg: 'nothing to say',
+        
+
+    }
+];
+
+let techKaro = {
+    faculty: [
+        {
+            name: "a",
+            age: "21",
+            
+        },
+        {
+            name: "a",
+            age: "21",
+            
+        }, {
+            name: "a",
+            age: "21",
+            
+        }, 
+        {
+            name: "a",
+            age: "21",
+            
+        }, 
+        {
+            name: "a",
+            age: "21",
+            
+        }  
+    ],
+    teachers: [
+        {
+            name: "a",
+            age: "21",
+            
+        },
+        {
+            name: "a",
+            age: "21",
+            
+        }, {
+            name: "a",
+            age: "21",
+            
+        }, 
+        {
+            name: "a",
+            age: "21",
+            
+        }, 
+        {
+            name: "a",
+            age: "21",
+            
+        }  
+    ],
+    ambassador: [
+        {
+            name: "a",
+            age: "21",
+            
+        },
+        {
+            name: "a",
+            age: "21",
+            
+        }, {
+            name: "a",
+            age: "21",
+            
+        }, 
+        {
+            name: "a",
+            age: "21",
+            
+        }, 
+        {
+            name: "a",
+            age: "21",
+            
+        }  
+    ],
+    students: [
+        {
+            name: "a",
+            age: "21",
+            
+        },
+        {
+            name: "a",
+            age: "21",
+            
+        }, {
+            name: "a",
+            age: "21",
+            
+        }, 
+        {
+            name: "a",
+            age: "21",
+            
+        }, 
+        {
+            name: "a",
+            age: "21",
+            
+        }  
+    ]
+};
